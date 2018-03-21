ea06b8a7b4e279ce7439c23f994b0fcdbfcd5ae5
Dmitry <dinvk@mail.ru>
2018-03-21 17:23:38 +0500

    align code according to codestyle

---
+++ app/pages/posts.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Post extends Component {
+    static getInitialProps({ query: { id } }) {
+        return { postId: id };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>My blog post #{this.props.postId}</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+            </div>
+        );
+    }
+}
+
+Post.propTypes = {
+    postId: PropTypes.number.isRequired
+};

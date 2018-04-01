a8752ad89ddedc225bb8f5fc83b8862d85125667
eyebrow elite <wivoceraku@p33.org>
2018-04-01 17:40:16 +0500

    +small spec

--- app/test/hrudb-client.spec.js
+++ app/test/hrudb-client.spec.js
@@ -7,10 +7,22 @@ describe('HrudbClient', async () => {
     const testToken = '8f92d8b92cffc5d2c4ddb2af9959dfa9391b6f43';
     const hrudb = new HrudbClient(testToken);
 
+    beforeEach(async () => {
+        await hrudb.remove('key');
+    })
+
     it('should put/get key-value', async () => {
         await hrudb.put('key', 'value');
         const value = await hrudb.get('key');
 
         expect(value).to.be.equal('value');
-    })
+    });
+
+    it('should post multiple values then getAll', async () => {
+        const expected = ['SanaraBoi', '}{"SanaraBoi', 'SanaraBoi2'];
+        await Promise.all(expected.map(x => hrudb.post('key', x)));
+        const all = await hrudb.getAll('key');
+
+        expect(all).to.be.deep.equal(expected)
+    });
 })

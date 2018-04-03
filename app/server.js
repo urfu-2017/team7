9e5e1c71fa08fc45993ea43efcf81a2a7c1c4223
Edheltur <kanaroooo@gmail.com>
2018-04-03 21:31:55 +0500

    Починил линт

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const next = require('next');
 const socketServer = require('./sockets/server');
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });

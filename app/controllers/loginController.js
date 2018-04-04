00fd8ac642776dd7b3203fabb3000b95b03da290
eyebrow elite <wivoceraku@p33.org>
2018-04-04 23:54:52 +0500

    Сохраняем юзера при логине

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,5 +1,7 @@
-import { Router } from 'express';
+import Router from 'express-promise-router';
 import connectEnsureLogin from 'connect-ensure-login';
+import { saveUser, getUser } from '../db/users-repository';
+import { User } from '../db/datatypes';
 
 
 export default passport => Router()
@@ -11,7 +13,16 @@ export default passport => Router()
     .get(
         '/login/return',
         passport.authenticate('github', { failureRedirect: '/' }),
-        (req, res) => res.redirect('/profile')
+        async (req, res) => {
+            const { username, _json: { id } } = req.user;
+            let user = await getUser(id);
+            if (!user) {
+                user = new User(id, username, null, []);
+                await saveUser(user);
+            }
+
+            res.json(user);
+        }
     )
     .get(
         '/profile',

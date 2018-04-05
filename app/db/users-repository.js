0dbba3357b7d93b030d857efbc131fe1f598befe
eyebrow elite <wivoceraku@p33.org>
2018-04-05 09:04:04 +0500

    + userRepo.createIfNotExists

--- app/db/users-repository.js
+++ app/db/users-repository.js
@@ -1,8 +1,19 @@
 import { post, getAll } from './hrudb-client';
+import { User } from './datatypes';
 
 export const saveUser = user => post('Users', user);
 export const getUser = async (userId) => {
     const users = await getAll('Users');
 
     return users.find(user => user.id === userId);
+};
+
+export const createIfNotExists = async (username, id) => {
+    let user = await getUser(id);
+    if (!user) {
+        user = new User(id, username, null, []);
+        await saveUser(user);
+    }
+
+    return user;
 };

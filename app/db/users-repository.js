03c996ffc812ab3704626ea55cfc32643f662886
eyebrow elite <wivoceraku@p33.org>
2018-04-10 11:50:17 +0500

    Делаем на один запрос меньше

--- app/db/users-repository.js
+++ app/db/users-repository.js
@@ -7,10 +7,10 @@ export const getAllUsers = () => get('AllUsers').catch(() => ({}));
 export const getUser = async userId => get(`Users_${userId}`);
 
 export const upsertUserWithIndex = async (updatedUser) => {
-    const user = await getUser(updatedUser.userId).catch(() => ({}));
+    const allUsers = await getAllUsers();
+    const user = allUsers[updatedUser.userId] || {};
     const nameChanged = user.username !== updatedUser.username;
     if (nameChanged) {
-        const allUsers = await getAllUsers();
         allUsers[updatedUser.userId] = updatedUser.username;
         await put('AllUsers', allUsers);
     }

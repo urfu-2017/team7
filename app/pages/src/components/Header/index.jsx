643319668f81109c1caad99ca6861842d6822bf7
Gusarov Vadim <gusarovvadim1996@gmail.com>
2018-04-11 02:44:59 +0500

    header, страница /user

--- app/pages/src/components/Header/index.jsx
+++ app/pages/src/components/Header/index.jsx
@@ -1,3 +1,18 @@
 import React from 'react';
+import { Header, Icon } from 'semantic-ui-react'
+import css from './styles.css'
 
-export default () => <div className="header">KILLOGRAM</div>;
+class MainHeader extends React.Component {
+    render(){
+        return(
+            <Header className={css.header} as='h1' icon textAlign='center' size="tiny">
+                <Icon className={css.header__image} circular />
+                <Header.Content>
+                    KILOGRAMM
+                </Header.Content>
+            </Header>
+        )
+    }
+}
+
+export default MainHeader
--- app/pages/src/components/User/index.jsx
+++ app/pages/src/components/User/index.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { observer } from 'mobx-react';
-import UserView from './User';
+import React from 'react'
+import { Grid, Button} from 'semantic-ui-react'
+import UserCard from '../UserCard'
+import css from './styles.css'
 
 class User extends React.Component {
-    render() {
-        return <UserView />;
+    render(){
+        return(
+            <Grid className={css.profileBox}>
+                <Grid.Row centered columns={7}>
+                    <Grid.Column />
+                    <Grid.Column />
+                    <Grid.Column />
+                    <Grid.Column>
+                        <UserCard />
+                    </Grid.Column>
+                    <Grid.Column />
+                    <Grid.Column />
+                    <Grid.Column />
+                </Grid.Row>
+                <Grid.Row centered columns={5}>
+                    <Grid.Column />
+                    <Grid.Column />
+                    <Grid.Column />  
+                    <Grid.Column>
+                        <form action="/logout">
+                            <Button secondary type="submit">
+                                LOGOUT
+                            </Button>
+                        </form>
+                    </Grid.Column>
+                    
+                    <Grid.Column /> 
+                </Grid.Row>
+            </Grid>
+        );
     }
 }
 
-export default observer(User);
+export default User

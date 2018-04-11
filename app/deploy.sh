a177606214548fee2d8f81598265d37be1e335ff
fng3r <dinvk@mail.ru>
2018-04-11 13:36:53 +0300

    remove previous deployment after successful switching to the current one

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-now rm team7chat -y --team team7chat --token=$NOW_TOKEN
 now --public --team team7chat --token=$NOW_TOKEN
 now alias --team team7chat --token=$NOW_TOKEN
+now rm team7chat -y --safe --team team7chat --token=$NOW_TOKEN

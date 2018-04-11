ecaaff314b4a48598b97f9723a7dc46e61a5d9fd
Gusarov Vadim <gusarovvadim1996@gmail.com>
2018-04-11 14:05:13 +0500

    Компоненты <User /> и <Header /> (#35)
    * header, страница /user
    
    * fixed code style
    
    * fix problems

--- app/pages/src/components/User/index.jsx
+++ app/pages/src/components/User/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
-import { observer } from 'mobx-react';
-import UserView from './User';
+import { Grid, Button } from 'semantic-ui-react';
+import UserCard from '../UserCard';
+import css from './styles.css';
 
 class User extends React.Component {
     render() {
-        return <UserView />;
+        return (
+            <Grid className={css.profileBox}>
+                <Grid.Row centered columns={3}>
+                    <Grid.Column width={3} />
+                    <Grid.Column width={2}>
+                        <UserCard />
+                    </Grid.Column>
+                    <Grid.Column width={3} />
+                </Grid.Row>
+                <Grid.Row centered columns={2}>
+                    <Grid.Column width={3} />
+                    <Grid.Column width={1}>
+                        <form action="/logout">
+                            <Button secondary type="submit">
+                                LOGOUT
+                            </Button>
+                        </form>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
     }
 }
 
-export default observer(User);
+export default User;

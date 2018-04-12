e7e4dd7975ee040b80a9581a2ee459f59bff393f
eyebrow elite <wivoceraku@p33.org>
2018-04-12 22:32:46 +0500

    Фикс бага

--- app/db/users-repository.js
+++ app/db/users-repository.js
@@ -1,8 +1,11 @@
-import { put, get } from './hrudb-repeater';
+import { put, get, getAll } from './hrudb-repeater';
 
 export const upsertUser = async updatedUser => put(`Users_${updatedUser.userId}`, updatedUser);
 
-export const getAllUsers = () => get('AllUsers').catch(() => ({}));
+export const getAllUsers = async () => {
+    const allUsersRef = await getAll('AllUsers');
+    return allUsersRef.length ? allUsersRef[0] : {};
+};
 
 export const getUser = async userId => get(`Users_${userId}`);
 

cf2b828a88bd151c727ebb8b9f1ca391fb8184a8
Edheltur <kanaroooo@gmail.com>
2018-04-18 13:11:11 +0500

    TEAMSEVEN-48: Emoji (#80)
    * Emoji
    
    * Emoji. th -> td
    
    * Emoji. linter
    
    * Emoji. поправил вёрсточный баг. Эмоджи больше не обрезаются в списке чатов.
    
    * Emoji. Починил версточный баг в Safari
    
    * Emoji. Hightlight на hover

--- app/pages/src/components/MessageInput/index.jsx
+++ app/pages/src/components/MessageInput/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
-import { Input, Button } from 'semantic-ui-react';
+import { Input, Button, Icon } from 'semantic-ui-react';
 import { sendMessage } from '../../../../sockets/client';
 import css from './layout.css';
 import { MAX_MESSAGE_LENGTH } from '../../../../utils/constants';
+import EmojiSelector from '../EmojiSelector';
 
 @inject('chatsStore')
 @observer
@@ -15,11 +16,26 @@ class MessageInput extends React.Component {
     }
 
 
-    handleChange = (e, { name, value }) => this.setState({ [name]: value });
+    handleChange = (e, { name, value }) => {
+        this.setState({ [name]: value });
+        this.handleCursor(e);
+    };
+
+    handleCursor = (e) => {
+        this.setState({ cursorPosition: e.target.selectionStart });
+    };
+
     handleKeyPress = (e) => {
         if (e.key === 'Enter') {
             this.trySend();
         }
+    };
+
+    handleEmojiSelect = (value) => {
+        const { text, cursorPosition } = this.state;
+        const before = text.substr(0, cursorPosition);
+        const after = text.substr(cursorPosition);
+        this.setState({ text: before + value + after });
     };
     trySend = () => {
         if (this.isValid) {
@@ -35,12 +51,25 @@ class MessageInput extends React.Component {
             <div className={css.layout}>
                 <Input
                     name="text"
+                    icon
                     value={this.state.text}
                     onChange={this.handleChange}
+                    onClick={this.handleCursor}
+                    onFocus={this.handleCursor}
                     className={css.layout__input}
                     onKeyPress={this.handleKeyPress}
                     maxLength={MAX_MESSAGE_LENGTH}
-                />
+                >
+                    <input />
+                    <EmojiSelector onSelected={this.handleEmojiSelect}>
+                        <Icon
+                            name="smile"
+                            size="large"
+                            color="grey"
+                            style={{ width: '2.2em', pointerEvents: 'auto', cursor: 'pointer' }}
+                        />
+                    </EmojiSelector>
+                </Input>
                 <Button
                     disabled={!this.isValid}
                     className={css.layout__button}

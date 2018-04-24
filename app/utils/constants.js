11a34cbd89e860dd2d78edcdecc139cdd3380fde
bottledeck <wivoceraku@p33.org>
2018-04-25 00:19:51 +0500

    Cache control для /static и /avatar

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -3,3 +3,4 @@ export const MAX_CHAT_NAME_LENGTH = 25;
 export const OWL_RANGE_START = 1;
 export const OWL_RANGE_END = 18;
 export const REPEATER_TIMES = 10;
+export const STATIC_MAX_AGE_SECONDS = 31536000;

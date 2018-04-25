f445b3f1d047e6c00d91b813c1dbd2a5b578535f
Gusarov Vadim <gusarovvadim1996@gmail.com>
2018-04-25 15:18:00 +0500

    добавил красный крестик при неудачной отправке

--- app/pages/src/components/Loader/index.jsx
+++ app/pages/src/components/Loader/index.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 class Loader extends React.Component {
     render() {
         const { status } = this.props;
-        if (status !== 'SENT') {
-            return '';
+        if (status === 'SENT') {
+            return (
+                <div className="ui active inline loader tiny" style={{ margin: '0 1em' }} />
+            );
+        } else if (status === 'REVOKED') {
+            return (
+                <div style={{ color: 'red', display: 'inline-block', margin: '0, 1em' }}>&#10006;</div>
+            );
         }
 
-        return (
-            <div className="ui active inline loader tiny" style={{ margin: '0 1em' }} />
-        );
+        return '';
     }
 }
 

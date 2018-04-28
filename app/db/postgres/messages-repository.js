91c3f76ee5fbb2de91b37f303fe49814592401e3
Admirallll <stanislav.yarkeev@gmail.com>
2018-04-28 21:37:26 +0500

    reactions backend

--- app/db/postgres/messages-repository.js
+++ app/db/postgres/messages-repository.js
@@ -1,8 +1,68 @@
 import { knex } from './knex';
 
-export const createMessage = async message => knex('message').insert(message);
-export const getMessagesFromChat = async (chatId) => {
-    const messages = await knex('message').where({ chatId });
+export const createMessage = async ({
+    messageId,
+    timestamp,
+    authorUserId,
+    content,
+    originalContent,
+    chatId
+}) => knex('message').insert({
+    messageId, timestamp, authorUserId, content, originalContent, chatId
+});
 
-    return messages.map(msg => ({ ...msg, timestamp: new Date(msg.timestamp) }));
+export const getMessagesFromChat = async (chatId) => {
+    const messages = await knex('message')
+        .select('message.*', 'reactions.userId', 'reactions.reaction')
+        .where({ chatId })
+        .leftJoin('reactions', 'reactions.messageId', 'message.messageId');
+
+    const reactions = messages.reduce((result, message) => {
+        if (!result[message.messageId]) {
+            result[message.messageId] = []; // eslint-disable-line no-param-reassign
+        }
+        if (message.userId) {
+            result[message.messageId].push({
+                userId: message.userId, reaction: message.reaction
+            });
+        }
+
+        return result;
+    }, {});
+
+    messages.forEach((msg) => {
+        delete msg.userId; // eslint-disable-line no-param-reassign
+        delete msg.reaction; // eslint-disable-line no-param-reassign
+    });
+
+    return messages.map(msg => ({
+        ...msg, timestamp: new Date(msg.timestamp), reactions: reactions[msg.messageId]
+    }));
 };
+
+export const getMessage = async (messageId) => {
+    const messages = await knex('message')
+        .select('message.*', 'reactions.userId', 'reactions.reaction')
+        .where({ 'message.messageId': messageId })
+        .leftJoin('reactions', 'reactions.messageId', 'message.messageId');
+
+    const reactions = messages.reduce((result, msg) => {
+        if (msg.userId) {
+            result.push({ userId: msg.userId, reaction: msg.reaction });
+        }
+        delete msg.userId; // eslint-disable-line no-param-reassign
+        delete msg.reaction; // eslint-disable-line no-param-reassign
+
+        return result;
+    }, []);
+
+    return messages.map(msg => ({
+        ...msg, timestamp: new Date(msg.timestamp), reactions
+    }))[0];
+};
+
+export const addReaction = async ({ messageId, userId, reaction }) =>
+    knex('reactions').insert({ messageId, userId, reaction });
+
+export const removeReaction = async ({ messageId, userId, reaction }) =>
+    knex('reactions').where({ messageId, userId, reaction }).del();
--- app/test/knex/repositories.spec.js
+++ app/test/knex/repositories.spec.js
@@ -89,11 +89,11 @@ test('can send messages to chat', async () => {
     await chatsRepo.joinChat(userId1, chat.chatId);
     const msg1 = new Message(
         '21111117-287a-46ec-8c67-8cc0c89eb77c', new Date(), userId1,
-        'privet1', 'privet1', chat.chatId
+        'privet1', 'privet1', chat.chatId, []
     );
     const msg2 = new Message(
         '31111117-287a-46ec-8c67-8cc0c89eb77c', new Date(), userId1,
-        'paka1', 'paka1', chat.chatId
+        'paka1', 'paka1', chat.chatId, []
     );
     await messagesRepo.createMessage(msg1);
     await messagesRepo.createMessage(msg2);
@@ -211,4 +211,53 @@ test('can create two different inviteWord on collision', async () => {
     expect(chat2.inviteWord).to.be.equal('yes-yes');
     expect(chat1).to.be.deep.equal(chat3);
     expect(chat2).to.be.deep.equal(chat4);
+});
+
+test('can add reaction', async () => {
+    const userId1 = await loginUser(user1.githubId, user1.username);
+    const reaction1 = 'reaction1';
+    const reaction2 = 'reaction2';
+    const chat = await chatsRepo.createChat('чятик', 'avatarUrl');
+    await chatsRepo.joinChat(userId1, chat.chatId);
+    const msg1 = new Message(
+        '21111117-287a-46ec-8c67-8cc0c89eb77c', new Date(), userId1,
+        'privet1', 'privet1', chat.chatId, []
+    );
+    await messagesRepo.createMessage(msg1);
+    await messagesRepo.addReaction({
+        messageId: msg1.messageId, userId: userId1, reaction: reaction1
+    });
+    await messagesRepo.addReaction({
+        messageId: msg1.messageId, userId: userId1, reaction: reaction2
+    });
+    const { reactions } = await messagesRepo.getMessage(msg1.messageId);
+
+    expect(reactions).to.be.deep.equal([
+        { userId: userId1, reaction: reaction1 },
+        { userId: userId1, reaction: reaction2 }
+    ]);
+});
+
+test('can remove reaction', async () => {
+    const userId1 = await loginUser(user1.githubId, user1.username);
+    const reaction1 = 'reaction1';
+    const chat = await chatsRepo.createChat('чятик', 'avatarUrl');
+    await chatsRepo.joinChat(userId1, chat.chatId);
+    const msg1 = new Message(
+        '21111117-287a-46ec-8c67-8cc0c89eb77c', new Date(), userId1,
+        'privet1', 'privet1', chat.chatId, []
+    );
+    await messagesRepo.createMessage(msg1);
+    await messagesRepo.addReaction({
+        messageId: msg1.messageId, userId: userId1, reaction: reaction1
+    });
+    const reactionsBeforeDelete = (await messagesRepo.getMessage(msg1.messageId)).reactions;
+
+    expect(reactionsBeforeDelete).to.be.deep.equal([{ userId: userId1, reaction: reaction1 }]);
+    await messagesRepo.removeReaction({
+        messageId: msg1.messageId, userId: userId1, reaction: reaction1
+    });
+    const reactionsAfterDelete = (await messagesRepo.getMessage(msg1.messageId)).reactions;
+
+    expect(reactionsAfterDelete).to.be.deep.equal([]);
 });

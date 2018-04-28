2b35b07da78f517d5de35b86fcfdbeee9a8062d2
fng3r <dinvk@mail.ru>
2018-04-28 15:01:46 +0300

    remove withRouter from Messages component

--- app/pages/src/components/Messages/index.jsx
+++ app/pages/src/components/Messages/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
 
 import Messages from './Messages';
 import DimmerLoader from '../DimmerLoader';
 
-@withRouter
 @inject('chatsStore', 'messagesStore', 'usersStore')
 @observer
 class MessageList extends React.Component {

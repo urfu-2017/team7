182323ff95d68b063fc202673d82e87122d7597c
bottledeck <wivoceraku@p33.org>
2018-04-30 01:49:58 +0500

    Правки по ревью

--- app/pages/src/components/Loader/index.jsx
+++ app/pages/src/components/Loader/index.jsx
@@ -4,13 +4,19 @@ import { Icon } from 'semantic-ui-react';
 class Loader extends React.Component {
     render() {
         const { status } = this.props;
+        const style = {
+            margin: '0 1em',
+            'font-size': '1.2rem',
+            height: '1rem',
+            width: '1rem'
+        };
         if (status === 'SENT') {
             return (
-                <div className="ui active inline loader tiny" style={{ margin: '0 1em' }} />
+                <Icon className="ui active inline loader" size="tiny" style={style} />
             );
         } else if (status === 'REVOKED') {
             return (
-                <Icon name="dont" size="large" color="red" style={{ margin: '0 0.5em' }} />
+                <Icon name="dont" size="large" color="red" style={style} />
             );
         }
 

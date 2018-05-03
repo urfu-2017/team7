34db256148c9a62f02b9edafb5fc0932e105f18d
levshin <M.Levshin@hostco.ru>
2018-05-03 23:28:44 +0500

    Будильник еще правки

--- app/pages/src/components/UserCard/index.jsx
+++ app/pages/src/components/UserCard/index.jsx
@@ -30,7 +30,7 @@ export default class UserCard extends React.Component {
                             <AvatarChanger />
                         </Card.Content>
                         <Card.Content extra>
-                            <a href="/#alarms"><Icon name="clock" />Ваши Будильники</a>
+                            <a href="/#alarms"><Icon name="clock" />Будильники</a>
                         </Card.Content>
                         <Card.Content extra>
                             <a href="/logout"><Icon name="rocket" />Выйти</a>

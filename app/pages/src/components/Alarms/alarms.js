4b0146249abd5f75e7a3db95dd93c31f21b9c1f8
levshin <M.Levshin@hostco.ru>
2018-05-05 14:11:19 +0500

    Будильники продолжение истории

--- app/pages/src/components/Alarms/alarms.js
+++ app/pages/src/components/Alarms/alarms.js
@@ -1,21 +1,42 @@
-import ArtasAdd from './files/Artas/add.mp3';
-import UndeadAdd from './files/Undead/add.mp3';
-
-import ArtasImg from './files/Artas/img.jpg';
-import UndeadImg from './files/Undead/img.jpg';
-
-import UndeadRemove from './files/Undead/remove.mp3';
-import ArtasRemove from './files/Artas/remove.mp3';
-
 import Artas0 from './files/Artas/0.mp3';
 import Artas1 from './files/Artas/1.mp3';
 import Artas2 from './files/Artas/2.mp3';
 import Artas3 from './files/Artas/3.mp3';
+import ArtasRemove from './files/Artas/remove.mp3';
+import ArtasAdd from './files/Artas/add.mp3';
+import ArtasImg from './files/Artas/img.jpg';
 
 import Undead0 from './files/Undead/0.mp3';
 import Undead1 from './files/Undead/1.mp3';
 import Undead2 from './files/Undead/2.mp3';
 import Undead3 from './files/Undead/3.mp3';
+import UndeadRemove from './files/Undead/remove.mp3';
+import UndeadAdd from './files/Undead/add.mp3';
+import UndeadImg from './files/Undead/img.jpg';
+
+import Succubus0 from './files/Succubus/0.mp3';
+import Succubus1 from './files/Succubus/1.mp3';
+import Succubus2 from './files/Succubus/2.mp3';
+import Succubus3 from './files/Succubus/3.mp3';
+import SuccubusRemove from './files/Succubus/remove.mp3';
+import SuccubusAdd from './files/Succubus/add.mp3';
+import SuccubusImg from './files/Succubus/img.jpg';
+
+import Thrall0 from './files/Thrall/0.mp3';
+import Thrall1 from './files/Thrall/1.mp3';
+import Thrall2 from './files/Thrall/2.mp3';
+import Thrall3 from './files/Thrall/3.mp3';
+import ThrallRemove from './files/Thrall/remove.mp3';
+import ThrallAdd from './files/Thrall/add.mp3';
+import ThrallImg from './files/Thrall/img.jpg';
+
+import CenaShort from './files/Cena/short.mp3';
+import CenaLong from './files/Cena/long.mp3';
+import CenaImg from './files/Cena/img.jpg';
+
+import DIMONShort from './files/DIMON/short.mp3';
+import DIMONLong from './files/DIMON/long.mp3';
+import DIMONImg from './files/DIMON/img.jpg';
 
 import Cant from './files/Common/cant.mp3';
 
@@ -27,7 +48,6 @@ export const alarmsInfo = [
         remove: ArtasRemove,
         sounds: [Artas0, Artas1, Artas2, Artas3],
         img: ArtasImg,
-        header: 'За Лордерон!',
         interval: 3000
     },
     {
@@ -37,8 +57,43 @@ export const alarmsInfo = [
         remove: UndeadRemove,
         sounds: [Undead0, Undead1, Undead2, Undead3],
         img: UndeadImg,
-        header: 'Мертвые идут!',
         interval: 3000
+    },
+    {
+        voice: 'Sucubus',
+        name: 'Суккуб',
+        add: SuccubusAdd,
+        remove: SuccubusRemove,
+        sounds: [Succubus0, Succubus1, Succubus2, Succubus3],
+        img: SuccubusImg,
+        interval: 2000
+    },
+    {
+        voice: 'Thrall',
+        name: 'Тралл',
+        add: ThrallAdd,
+        remove: ThrallRemove,
+        sounds: [Thrall0, Thrall1, Thrall2, Thrall3],
+        img: ThrallImg,
+        interval: 2000
+    },
+    {
+        voice: 'Cena',
+        name: 'Джон Сина',
+        add: CenaShort,
+        remove: CenaShort,
+        sounds: [CenaLong],
+        img: CenaImg,
+        interval: 10000
+    },
+    {
+        voice: 'DIMON',
+        name: 'ДИМОООООН',
+        add: DIMONShort,
+        remove: DIMONShort,
+        sounds: [DIMONLong],
+        img: DIMONImg,
+        interval: 10000
     }
 ];
 
@@ -60,7 +115,6 @@ export const stopSound = () => {
 
 export const playSoundSrc = (src) => {
     stopSound();
-    // eslint-disable-next-line
     sound = new Audio(src);
     sound.play();
 };
